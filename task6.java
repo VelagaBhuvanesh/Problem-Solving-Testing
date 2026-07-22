3873c5deb5d11e7a9750abf3b4bbc40588a416e6
Velaga_Bhuvanesh <velagabhuvanesh2@gmail.com>
2026-07-22 04:34:13 +0000

    task6

---
+++ task6.java
@@ -0,0 +1,18 @@
+public class task6 {
+    public static void printAllPairs(int[] arr) {
+        int n = arr.length;
+        System.out.println("Printing all ordered pairs (including self-pairs):");
+
+        // Both loops run through the entire array
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.println("(" + arr[i] + ", " + arr[j] + ")");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3};
+        printAllPairs(arr);
+    }
+}
