09d21f4a6dfa14c9562a4701207c8c56fcd7c019
Velaga_Bhuvanesh <velagabhuvanesh2@gmail.com>
2026-07-29 04:09:49 +0000

    practice ques

---
+++ SalesApp.java
@@ -0,0 +1,24 @@
+import java.util.function.Predicate;
+
+public class SalesApp {
+    public static void main(String[] args) {
+        
+        Predicate<Double> isHighValue = amount -> amount >= 10000.0; 
+
+        
+  
+        if (isHighValue == null) {
+            System.out.println(" Please replace 'null' with your lambda expression to begin!");
+            return;
+        }
+
+        double order1 = 12500.0;
+        double order2 = 4500.0;
+        double order3 = 10000.0;
+
+        System.out.println("--- Testing Sales Lambda ---");
+        System.out.println("Order of ₹" + order1 + " High Value? -> " + isHighValue.test(order1) + " (Expected: true)");
+        System.out.println("Order of ₹" + order2 + " High Value? -> " + isHighValue.test(order2) + " (Expected: false)");
+        System.out.println("Order of ₹" + order3 + " High Value? -> " + isHighValue.test(order3) + " (Expected: true)");
+    }
+}
